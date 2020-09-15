e0fbbd57f38a7d6e62fae9b68f7c526dbd38b819
Sumi Sastri <Sumitra.Sastri@gvcgroup.com>
2020-09-15 16:48:27 +0100

    opens 2 modals

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,18 +1,14 @@
 const openModalButtons = document.querySelectorAll('[data-modal-target]');
+console.log(openModalButtons);
 const closeModalButtons = document.querySelectorAll('[data-close-button]');
-const overlay = document.getElementById('overlay');
+console.log(closeModalButtons);
 
 openModalButtons.forEach((button) => {
   button.addEventListener('click', () => {
     const modal = document.querySelector(button.dataset.modalTarget);
-    openModal(modal);
-  });
-});
 
-overlay.addEventListener('click', () => {
-  const modals = document.querySelectorAll('.modal.active');
-  modals.forEach((modal) => {
-    closeModal(modal);
+    openModal(modal);
+    console.log(modal);
   });
 });
 
@@ -26,14 +22,13 @@ closeModalButtons.forEach((button) => {
 function openModal(modal) {
   if (modal == null) return;
   modal.classList.add('active');
-  overlay.classList.add('active');
 }
 
 function closeModal(modal) {
   if (modal == null) return;
   modal.classList.remove('active');
-  overlay.classList.remove('active');
 }
+
 let sliderImages = document.querySelectorAll('.slide'),
   arrowLeft = document.querySelector('#arrow-left'),
   arrowRight = document.querySelector('#arrow-right'),
