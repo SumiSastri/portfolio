5d2a1751b43bc16da985acd1dba815d796385f93
Sumi Sastri <Sumitra.Sastri@gvcgroup.com>
2020-09-16 11:30:00 +0100

    adds contact details

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -27,53 +27,54 @@ function closeModal(modal) {
   if (modal == null) return;
   modal.classList.remove('active');
 }
-
+// select nodes by class name and by ids
 let sliderImages = document.querySelectorAll('.slide'),
   arrowLeft = document.querySelector('#arrow-left'),
   arrowRight = document.querySelector('#arrow-right'),
   current = 0;
 
-// Clear all images
+// Function declaration - remove display of image when shuffle through images
 function reset() {
   for (let i = 0; i < sliderImages.length; i++) {
     sliderImages[i].style.display = 'none';
   }
 }
 
-// Init slider
+// Function call - reset function first clear images then display first
 function startSlide() {
   reset();
   sliderImages[0].style.display = 'block';
 }
 
-// Show prev
+// Function call - reset function - function declaration left arrow minus indices
 function slideLeft() {
   reset();
   sliderImages[current - 1].style.display = 'block';
   current--;
 }
 
-// Show next
+// Function call - reset function - function declaration right arrow - plus indices
 function slideRight() {
   reset();
   sliderImages[current + 1].style.display = 'block';
   current++;
 }
 
-// Left arrow click
+// Left arrow click function call of the slide left
 arrowLeft.addEventListener('click', function () {
+  // first check position in the node list - is it at zero - starting point
   if (current === 0) {
     current = sliderImages.length;
   }
   slideLeft();
 });
 
-// Right arrow click
+// Right arrow click function call of the slide right - reverses the image back to start if current is at the last image then slide right
 arrowRight.addEventListener('click', function () {
   if (current === sliderImages.length - 1) {
     current = -1;
   }
   slideRight();
 });
-
+// function call of start slide
 startSlide();
